64fe424269ab4ebd7e4a120fdc2fa40ac6fd5ed3
vinaybommana7 <vinaybommana7@gmail.com>
2017-09-29 23:53:25 +0530

    added wiki's explanation

--- cron.py
+++ cron.py
@@ -30,6 +30,16 @@ class Task():
     days (1 - 31)
     day_of_week (0 - 6)
 
+     ┌───────────── minute (0 - 59)
+     │ ┌───────────── hour (0 - 23)
+     │ │ ┌───────────── day of month (1 - 31)
+     │ │ │ ┌───────────── month (1 - 12)
+     │ │ │ │ ┌───────────── day of week (0 - 6) (Sunday to Saturday;
+     │ │ │ │ │                          7 is also Sunday on some systems)
+     │ │ │ │ │
+     │ │ │ │ │
+     * * * * *  command to execute
+
     * * * * *
     # tells us that do the task now
     0 0 1 * *
